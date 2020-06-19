1eb8f591c590f5f59051aef6a9d312fd712a4e40
Nv7z2 <barankacper2000@gmail.com>
2020-06-19 22:08:57 +0200

    [added] Navigation component

---
+++ client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" data-theme="dark">
+    <navigation />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Navigation from './components/core/Navigation';
+
+export default {
+  components: {
+    Navigation
+  }
+};
+</script>
+
+<style lang="scss">
+@import './style/_base';
+
+#app {
+  height: 100vh;
+  background-color: $backgroundColor;
+}
+</style>
---
+++ client/src/components/core/Navigation.vue
@@ -0,0 +1,123 @@
+<template>
+  <nav class="navigation" :class="{ 'navigation--active': isMenuAvtive }">
+    <button
+      class="navigation__hamburger"
+      @click="isMenuAvtive = !isMenuAvtive"
+      :class="{ 'navigation__hamburger--active': isMenuAvtive }"
+    >
+      <span class="navigation__hamburger-line"></span>
+      <span class="navigation__hamburger-line"></span>
+      <span class="navigation__hamburger-line"></span>
+    </button>
+
+    <ul class="navigation__list">
+      <li
+        class="navigation__element"
+        v-for="category in navCategories"
+        :key="category"
+        v-show="isMenuAvtive"
+      >
+        <router-link to="#" class="navigation__link">{{ category }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isMenuAvtive: false,
+      navCategories: ['Private notes', 'Work notes', 'Other notes']
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+  padding: 24px;
+  width: 80px;
+  background-color: transparent;
+  transition: width 0.2s ease-in-out, background 0.2s ease-in-out;
+
+  @include mq {
+    height: 100vh;
+    background-color: $navigationBackground;
+  }
+
+  &--active {
+    width: 100%;
+    height: 100%;
+    background-color: $navigationBackground;
+
+    @include mq {
+      width: 400px;
+    }
+  }
+
+  &__hamburger {
+    width: 32px;
+    height: 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  &__hamburger-line {
+    display: block;
+    width: 100%;
+    height: 3px;
+    background-color: #eee;
+    transition: transform 0.2s ease-in-out, opacity 0.3s ease-in-out;
+
+    &:nth-of-type(2) {
+      width: 75%;
+    }
+  }
+
+  &__hamburger--active &__hamburger-line {
+    &:nth-child(1) {
+      transform: rotate(45deg) translate(0.45rem, 0.5rem);
+    }
+    &:nth-child(2) {
+      opacity: 0;
+      transform: translateX(0.5rem);
+    }
+    &:nth-child(3) {
+      transform: rotate(-45deg) translate(0.45rem, -0.5rem);
+    }
+  }
+
+  &__list {
+    margin-top: 5rem;
+    opacity: 0;
+    transition: all 0.2s ease-in-out;
+  }
+
+  &--active &__list {
+    opacity: 1;
+  }
+
+  &__element {
+  }
+
+  &__link {
+    display: block;
+    color: #eee;
+    padding: 0.5rem;
+    opacity: 0;
+    font-weight: bold;
+    white-space: nowrap;
+    transition: border-left 0.1s ease-in-out, opacity 0.3s ease-in-out;
+
+    &:hover {
+      border-left: 2px solid #eee;
+    }
+  }
+
+  &--active &__link {
+    opacity: 1;
+  }
+}
+</style>
