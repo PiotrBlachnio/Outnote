99b28f97771d7959453ceed5bea9f145ce054351
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-19 20:54:48 +0200

    Create slow down middleware

--- server/src/loaders/express.ts
+++ server/src/loaders/express.ts
@@ -1,7 +1,9 @@
 import { Application, json } from 'express';
+import renderRoutes from './render-routes';
 
 export default (app: Application): void => {
     app.use(json({ limit: '10kb' }));
-
     app.set('trust proxy', 'uniquelocal');
+
+    renderRoutes(app);
 };
