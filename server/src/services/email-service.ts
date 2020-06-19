a1792791a511b0d4c13b701b912d90572b5c0c81
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-19 21:50:24 +0200

    Create email service

---
+++ server/src/services/email-service.ts
@@ -0,0 +1,50 @@
+import nodemailer, { Transporter } from "nodemailer";
+import config from "../assets/config";
+import logger from "../utils/logger";
+import { Mail } from "../assets/enums";
+import MailTemplate from "../assets/mail-template";
+import { TMailData, IConfirmEmailTemplateData, IConfirmIdentityTemplateData, IResetPasswordTemplateData } from "../types/mails";
+
+const defaultTransporter: Transporter = nodemailer.createTransport({
+    pool: true,
+    service: 'Gmail',
+    auth: {
+        type: 'OAuth2',
+        user: config.MAIL_USER!,
+        refreshToken: config.MAIL_REFRESH_TOKEN!,
+        clientId: config.MAIL_CLIENT_ID!,
+        clientSecret: config.MAIL_CLIENT_SECRET!
+    }
+});
+
+class EmailService {
+    private readonly _transporter: Transporter;
+    
+    constructor(transporter: Transporter = defaultTransporter) {
+        this._transporter = transporter;
+
+        this._transporter.verify(async (error) => {
+            if(error) {
+                await logger.log({ type: 'error', message: error.message, place: 'Send mail function' });
+            };
+        });
+    };
+
+    public async sendMail(template: Mail, data: TMailData): Promise<void> {
+        switch(template) {
+            case Mail.CONFIRM_EMAIL:
+                this._transporter.sendMail(new MailTemplate().confirmEmail(data as IConfirmEmailTemplateData));
+                break;
+            case Mail.CONFIRM_IDENTITY:
+                this._transporter.sendMail(await new MailTemplate().confirmIdentity(data as IConfirmIdentityTemplateData));
+                break;
+            case Mail.RESET_PASSWORD:
+                this._transporter.sendMail(new MailTemplate().resetPassword(data as IResetPasswordTemplateData));
+                break;
+            default:
+                throw new Error('Template type is invalid!');
+        };
+    };
+};
+
+export default EmailService;
---
+++ server/src/types/index.d.ts
@@ -0,0 +1,30 @@
+import UserService from "../services/user-service";
+import EmailService from "../services/email-service";
+import BanService from "../services/ban-service";
+import TokenService from "../services/token-service";
+import EventEmitter from "../events/event-emitter";
+
+export {};
+
+declare module 'express-serve-static-core' {
+    interface Request {
+       user?: {
+          id?: string,
+          username?: string,
+          email?: string;
+       }
+       context?: {
+           username?: string;
+           email?: string;
+           password?: string;
+           id?: string;
+           banId?: string;
+       },
+       services: {
+           email: EmailService;
+           token: TokenService;
+           user: UserService;
+       },
+       eventEmitter: EventEmitter
+    }
+}
