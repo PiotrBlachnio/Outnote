8da4ee71157c1a38cc271c7c147c1976df0298f6
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-21 12:34:52 +0200

    Create event emitter

---
+++ server/src/events/event-emitter.ts
@@ -0,0 +1,19 @@
+import { EventEmitter as NodeEventEmitter } from 'events';
+import { TEvents } from './lib';
+
+export class EventEmitter extends NodeEventEmitter {
+    private _events: TEvents;
+
+    constructor(events: TEvents) {
+        super();
+
+        this._events = events;
+        this.initEvents();
+    };
+
+    private initEvents(): void {
+        this.on('REGISTER_SUCCESS', this._events.registerSuccess);
+    };
+};
+
+export default EventEmitter;
