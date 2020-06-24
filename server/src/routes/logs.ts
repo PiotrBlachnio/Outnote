1ae59c29d6e0d5fb2bdeac2f533cc3a618ac1dfa
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-24 12:46:04 +0200

    Create remove logs route

--- server/src/routes/logs.ts
+++ server/src/routes/logs.ts
@@ -10,7 +10,19 @@ const router: Router = Router();
 */
 router.get('/', async (req: Request, res: Response): Promise<Response<any> | void> => {
     const logs: object[] = await logger.findLogs({});
+
     res.status(200).json(logs);
+});
+
+/**
+ * @route   /logs
+ * @desc    Return all server logs
+ * @access  Public
+*/
+router.delete('/', async (req: Request, res: Response): Promise<Response<any> | void> => {
+    await logger.removeLogs();
+
+    res.status(200).end();
 });
 
 export default router;
