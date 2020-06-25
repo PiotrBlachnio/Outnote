492b3bff1989216e67256902b514a7ab7a083153
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-25 21:45:01 +0200

    Create validator for confirm-email route

--- server/src/middlewares/validators/index.ts
+++ server/src/middlewares/validators/index.ts
@@ -1,9 +1,11 @@
 import login from './login';
 import register from './register';
 import addLocation from './add-location';
+import confirmEmail from './confirm-email';
 
 export default {
     login,
     register,
-    addLocation
+    addLocation,
+    confirmEmail
 };
