3f3b24ae1b28541f77a2efdb536a5c720abfcdac
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-25 22:01:51 +0200

    Create validator for refresh-token route

--- server/src/middlewares/validators/index.ts
+++ server/src/middlewares/validators/index.ts
@@ -3,11 +3,13 @@ import register from './register';
 import addLocation from './add-location';
 import confirmEmail from './confirm-email';
 import forgotPassword from './forgot-password';
+import refreshToken from './refresh-token';
 
 export default {
     login,
     register,
     addLocation,
     confirmEmail,
-    forgotPassword
+    forgotPassword,
+    refreshToken
 };
