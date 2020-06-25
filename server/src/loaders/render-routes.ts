59e3b38a0cc02bf78dc9b1df2e217771f69c925b
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-25 21:38:31 +0200

    Expose add-location route to public access

--- server/src/loaders/render-routes.ts
+++ server/src/loaders/render-routes.ts
@@ -5,13 +5,14 @@ import config from '../assets/config';
 export default (app: Application): void => {
     const rootRouter: { path: string, router: Router }[] = [
         { path: 'login', router: routes.login },
-        { path: 'register', router: routes.register }
+        { path: 'register', router: routes.register },
+        { path: 'add-location', router: routes.addLocation },
+        { path: 'logs', router: routes.logs }
     ];
 
     rootRouter.forEach(object => {
         app.use(`${config.API_PATH}/${object.path}`, object.router);
     });
     
-    app.use('/logs', routes.logs);
     app.use('*', routes.defaultRoute);
 };
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,10 +2,12 @@ import defaultRoute from './default';
 import login from './login';
 import register from './register';
 import logs from './logs';
+import addLocation from './add-location';
 
 export default {
     defaultRoute,
     login,
     register,
-    logs
+    logs,
+    addLocation
 };
