454bc9d1ad8cf53e9ea5a159247142acf0ac2fee
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-06-25 21:28:36 +0200

    Create validator for add-location route

--- server/src/middlewares/validators/index.ts
+++ server/src/middlewares/validators/index.ts
@@ -1,7 +1,9 @@
 import login from './login';
 import register from './register';
+import addLocation from './add-location';
 
 export default {
     login,
-    register
+    register,
+    addLocation
 };
