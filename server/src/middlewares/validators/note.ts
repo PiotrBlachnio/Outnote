575b351950b541a55bae38fbe2c38c769ea76dfd
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-13 20:16:32 +0200

    Move create note validation login to a separate file

--- server/src/middlewares/validators/note.ts
+++ server/src/middlewares/validators/note.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import validator from '../../utils/validators';
-import { NoteNotFoundError } from "../../assets/errors";
-import { INote } from "../../types/models";
+import { NoteNotFoundError, IncorrectInputError, CategoryNotFoundError } from "../../assets/errors";
+import { INote, ICategory } from "../../types/models";
 
 async function getById(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
@@ -22,6 +22,34 @@ async function getById(req: Request, res: Response, next: NextFunction): Promise
     };
 };
 
+async function create(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const { title, categoryId, isPrivate } = req.body;
+
+        if(!validator.validateInput({ id: categoryId, title, isPrivate })) {
+            throw new IncorrectInputError;
+        };
+
+        const category: ICategory | null = await req.services.category.findById(categoryId);
+        if(!category) {
+            throw new CategoryNotFoundError;
+        };
+
+        const note: INote = await req.services.note.create({
+            title,
+            categoryId,
+            isPrivate,
+            ownerId: req.user!.id,
+        });
+
+        req.context = { note };
+    } catch(error) {
+        error.place = 'Get note by id route';
+        next(error);
+    };
+};
+
 export default {
-    getById
+    getById,
+    create
 }
