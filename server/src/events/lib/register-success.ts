552eae63dc6d92fb6b1e6fe32be38c3e099144dc
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-13 20:27:44 +0200

    Simplify events code

--- server/src/events/lib/register-success.ts
+++ server/src/events/lib/register-success.ts
@@ -5,32 +5,26 @@ import TokenService from "../../services/token-service";
 import { Token, Mail } from "../../assets/enums";
 import EmailService from "../../services/email-service";
 
-interface IServices {
-    userService: UserService,
-    tokenService: TokenService,
-    emailService: EmailService
+const defaultServices = {
+    user: new UserService(),
+    token: new TokenService(),
+    email: new EmailService()
 };
 
-const defaultServices: IServices = {
-    userService: new UserService(),
-    tokenService: new TokenService(),
-    emailService: new EmailService()
-};
-
-export default async (data: { username: string, email: string, password: string, ip: string }, services: IServices = defaultServices): Promise<void> => {
+export default async (data: { username: string, email: string, password: string, ip: string }, services: typeof defaultServices = defaultServices): Promise<void> => {
     const hashedPassword: string = await bcrypt.hash(data.password, 12);
     const hashedIP: string = await bcrypt.hash(data.ip, 12);
 
-    const user: IUser = await services.userService.create({
+    const user: IUser = await services.user.create({
         username: data.username,
         email: data.email,
         password: hashedPassword,
         trustedIPS: [hashedIP]
     });
 
-    const token: string = services.tokenService.generateToken(Token.CONFIRM_EMAIL, { id: user.id });
+    const token: string = services.token.generateToken(Token.CONFIRM_EMAIL, { id: user.id });
 
-    services.emailService.sendMail(Mail.CONFIRM_EMAIL, {
+    services.email.sendMail(Mail.CONFIRM_EMAIL, {
         email: data.email,
         id: user.id,
         token: token 
