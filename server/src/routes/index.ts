7a594d9794c1a5b99cb3a104d1b0cada3a14d64e
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-14 20:29:18 +0200

    Group auth routes into one file

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,29 +1,25 @@
 import defaultRoute from './default';
-import login from './login';
-import register from './register';
 import logs from './logs';
 import addLocation from './add-location';
 import confirmEmail from './confirm-email';
 import forgotPassword from './forgot-password';
-import logout from './logout';
 import resetPassword from './reset-password';
 import sendConfirmationMail from './send-confirmation-mail';
 import note from './note';
 import user from './user';
 import category from './category';
+import auth from './auth';
 
 export default {
     defaultRoute,
-    login,
-    register,
     logs,
     addLocation,
     confirmEmail,
     forgotPassword,
-    logout,
     resetPassword,
     sendConfirmationMail,
     note,
     category,
-    user
+    user,
+    auth
 };
