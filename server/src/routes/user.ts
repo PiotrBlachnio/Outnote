668066e16153f31d767fd8c4614172886f8fa258
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-14 20:07:23 +0200

    Create UPDATE_USER_SUCCESS event

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -18,7 +18,7 @@ router.use(cookieParser());
 router.patch('/avatar', auth(Roles.USER), new FileService().middleware.single('avatar'), async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     try {
         const filename: string = await req.services.file.uploadImage(req.file.buffer);
-        await req.services.user.updateOne({ _id: req.user!.id }, { avatar: filename });
+        req.eventEmitter.emit('UPDATE_USER_SUCCESS', req.user!.id, { avatar: filename });
 
         await logger.log({ type: 'info', message: 'Avatar updated successfully!', place: 'Update avatar route' });
         res.status(200).json({ filename });
