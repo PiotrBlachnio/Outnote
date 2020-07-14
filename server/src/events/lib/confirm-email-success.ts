ec6f117c82734b0e94319ea15d45b37d989e499d
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-14 21:07:16 +0200

    Group events related to the account API into one file

--- server/src/events/lib/confirm-email-success.ts
+++
@@ -1,9 +0,0 @@
-import UserService from "../../services/user-service";
-
-const defaultServices = {
-    user: new UserService()
-};
-
-export default async (userId: string, services: typeof defaultServices = defaultServices): Promise<void> => {
-    await services.user.updateOne({ _id: userId }, { isConfirmed: true });
-};
