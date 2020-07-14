d2722d09d52da37ba99cea62da527d7cabb7dcd6
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-14 20:38:32 +0200

    Group auth validators into one file

--- server/src/middlewares/validation/index.ts
+++ server/src/middlewares/validation/index.ts
@@ -1,5 +1,3 @@
-import login from './lib/login';
-import register from './lib/register';
 import addLocation from './lib/add-location';
 import confirmEmail from './lib/confirm-email';
 import forgotPassword from './lib/forgot-password';
@@ -7,10 +5,10 @@ import resetPassword from './lib/reset-password';
 import sendConfirmationMail from './lib/send-confirmation-mail';
 import note from './lib/note';
 import category from './lib/category';
+import auth from './lib/auth-validators';
 
 export default {
-    login,
-    register,
+    auth,
     addLocation,
     confirmEmail,
     forgotPassword,
