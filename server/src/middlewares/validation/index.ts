00e8cdec839b44df8b03870302541bd8558f6281
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-19 15:39:59 +0200

    Create update user route

--- server/src/middlewares/validation/index.ts
+++ server/src/middlewares/validation/index.ts
@@ -2,10 +2,12 @@ import account from './lib/account-validators';
 import note from './lib/note-validators';
 import category from './lib/category-validators';
 import auth from './lib/auth-validators';
+import user from './lib/user-validators';
 
 export default {
     auth,
     account,
     note,
-    category
+    category,
+    user
 };
