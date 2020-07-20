93642a338d5cb4a3da3579454aca019f6b6441bd
nv7z2 <barankacper2000@gmail.com>
2020-07-20 13:06:44 +0100

    [added] NoteThumbnail dropdown menu

--- client/src/components/dashboard/NoteThumbnail.vue
+++ client/src/components/dashboard/NoteThumbnail.vue
@@ -1,26 +1,90 @@
 <template>
-  <router-link
-    class="note-thumbnail"
-    :to="`/dashboard/note/${note.categoryId}/${note._id}`"
-  >
-    <span class="note-thumbnail__title">{{ note.title }}</span>
-    <span class="note-thumbnail__content">{{ noteContent }}</span>
+  <div class="note-thumbnail" @mouseleave="isDropdownActive = false">
+    <router-link
+      class="note-thumbnail__redirect"
+      :to="`/dashboard/note/${note.categoryId}/${note._id}`"
+    >
+      <span class="note-thumbnail__title">{{ note.title }}</span>
+      <span class="note-thumbnail__content">{{ noteContent }}</span>
 
-    <div class="note-thumbnail__tags">
-      <span class="note-thumbnail__tag" v-for="tag in note.tags" :key="tag">
-        #{{ tag }}
-      </span>
+      <div class="note-thumbnail__tags">
+        <span class="note-thumbnail__tag" v-for="tag in note.tags" :key="tag">
+          #{{ tag }}
+        </span>
+      </div>
+    </router-link>
+
+    <div class="note-thumbnail__dropdown">
+      <button
+        class="note-thumbnail__dropdown-toggle-button"
+        @click="toggleDropdown()"
+      >
+        <i class="fas fa-ellipsis-h"></i>
+      </button>
+
+      <ul
+        class="note-thumbnail__dropdown-menu"
+        :class="{ 'note-thumbnail__dropdown-menu--active': isDropdownActive }"
+      >
+        <li class="note-thumbnail__dropdown-element">
+          <button class="note-thumbnail__dropdown-button">
+            <i
+              class="fas fa-edit 
+            note-thumbnail__dropdown-icon 
+            note-thumbnail__dropdown-icon--edit"
+            ></i>
+            <span>Edit</span>
+          </button>
+        </li>
+        <li class="note-thumbnail__dropdown-element">
+          <button class="note-thumbnail__dropdown-button">
+            <i
+              class="fas fa-heading
+            note-thumbnail__dropdown-icon 
+            note-thumbnail__dropdown-icon--edit"
+            ></i>
+            <span>Edit title</span>
+          </button>
+        </li>
+        <li class="note-thumbnail__dropdown-element">
+          <button class="note-thumbnail__dropdown-button">
+            <i
+              class="fas fa-tags 
+            note-thumbnail__dropdown-icon 
+            note-thumbnail__dropdown-icon--edit"
+            ></i>
+            <span>Edit tags</span>
+          </button>
+        </li>
+        <li class="note-thumbnail__dropdown-element">
+          <button class="note-thumbnail__dropdown-button">
+            <i
+              class="fas fa-trash 
+            note-thumbnail__dropdown-icon 
+            note-thumbnail__dropdown-icon--remove"
+            ></i>
+            <span>Remove</span>
+          </button>
+        </li>
+      </ul>
     </div>
-  </router-link>
+  </div>
 </template>
 
 <script>
+import gsap from 'gsap';
+
 export default {
   props: {
     note: {
       type: Object,
       required: true
     }
+  },
+  data() {
+    return {
+      isDropdownActive: false
+    };
   },
   computed: {
     noteContent() {
@@ -30,24 +94,39 @@ export default {
         ? content.substr(0, 64).concat(' [...]')
         : content;
     }
+  },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownActive = !this.isDropdownActive;
+
+      gsap.fromTo(
+        '.note-thumbnail__dropdown-element',
+        this.isDropdownActive ? 0.1 : 0,
+        { opacity: 0, x: '-0.75rem' },
+        { stagger: 0.1, opacity: 1, x: 0 }
+      );
+    }
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .note-thumbnail {
-  cursor: pointer;
-  width: 100%;
-  color: #eee;
-  display: flex;
-  flex-direction: column;
-  padding: 1rem;
-  margin-bottom: 1rem;
-  border-radius: 0.25rem;
-  background-color: #141414;
+  &__redirect {
+    cursor: pointer;
+    width: 100%;
+    color: #eee;
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    position: relative;
+    margin-bottom: 1rem;
+    border-radius: 0.25rem;
+    background-color: #141414;
 
-  &:first-of-type {
-    margin-top: 1rem;
+    &:first-of-type {
+      margin-top: 1rem;
+    }
   }
 
   &__title {
@@ -71,6 +150,57 @@ export default {
     margin-right: 0.25rem;
     border-radius: 0.25rem;
     background-color: purple;
+  }
+
+  &__dropdown {
+    top: 0;
+    right: 0.5rem;
+    z-index: 10;
+    opacity: 0;
+    text-align: right;
+    padding: 0.5rem;
+    position: absolute;
+    transition: opacity 0.2s;
+  }
+
+  &:hover &__dropdown {
+    opacity: 1;
+  }
+
+  &__dropdown-toggle-button {
+    color: #eee;
+  }
+
+  &__dropdown-menu {
+    max-height: 0;
+    overflow-y: hidden;
+
+    &--active {
+      max-height: 100px;
+    }
+  }
+
+  &__dropdown-element {
+    color: #eee;
+    text-align: left;
+    font-size: 0.75rem;
+    border-radius: 0.25rem;
+    margin-bottom: 1px;
+    background-color: rgb(34, 34, 34);
+  }
+
+  &__dropdown-button {
+    width: 100%;
+    text-align: left;
+    padding: 0.25rem 0.5rem;
+  }
+
+  &__dropdown-icon {
+    margin-right: 0.5rem;
+
+    &--remove {
+      color: $error;
+    }
   }
 }
 </style>
