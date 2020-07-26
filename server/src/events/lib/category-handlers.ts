10bb7e8933bd91e3e16eeb5c3ca0d9f10a0d07ab
PiotrBlachnio <piotr.blachnio@yahoo.com>
2020-07-26 13:15:43 +0200

    Fix service methods

--- server/src/events/lib/category-handlers.ts
+++ server/src/events/lib/category-handlers.ts
@@ -5,11 +5,11 @@ const defaultServices = {
 };
 
 async function deleteCategorySuccessHandler(id: string, services: typeof defaultServices = defaultServices): Promise<void> {
-    await services.category.deleteOne({ id });
+    await services.category.deleteOne({ _id: id });
 };
 
 async function updateCategorySuccessHandler(id: string, updatedData: Record<string, unknown>, services: typeof defaultServices = defaultServices): Promise<void> {
-    await services.category.updateOne({ id }, updatedData);
+    await services.category.updateOne({ _id: id }, updatedData);
 };
 
 export default {
