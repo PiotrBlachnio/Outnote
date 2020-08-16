90a2cc5116a142dfbc11cc481dee260a56a1d8f4
nv7z2 <barankacper2000@gmail.com>
2020-08-16 19:51:14 +0200

    [changed] Separated Navigation into components

--- client/src/components/core/Navigation.vue
+++ client/src/components/core/Navigation.vue
@@ -5,39 +5,16 @@
       v-model="renameData.categoryName"
     />
 
-    <button
-      class="navigation__hamburger"
-      @click="closeNavigation"
-      :class="{ 'navigation__hamburger--active': isMenuActive }"
-    >
-      <span class="navigation__hamburger-line"></span>
-      <span class="navigation__hamburger-line"></span>
-      <span class="navigation__hamburger-line"></span>
-    </button>
+    <hamburger @click="closeNavigation" :isMenuActive="isMenuActive" />
 
-    <div class="navigation__add-category-wrapper" v-show="isMenuActive">
-      <button
-        @click="showAddCategoryInput"
-        to="#"
-        class="navigation__add-category-link"
-      >
-        <i class="fas fa-plus navigation__icon"></i> New
-      </button>
-
-      <div
-        v-show="isAddCategoryActive"
-        class="navigation__add-category-input-wrapper"
-      >
-        <base-input
-          id="newCategory"
-          less-padding
-          label="New category..."
-          v-model="newCategoryName"
-          @keypressEnter="addNewCategory"
-        />
-        <button @click="addNewCategory"><i class="fas fa-save"></i></button>
-      </div>
-    </div>
+    <add-category-field
+      v-show="isMenuActive"
+      :is-visible="isCategoryFieldVisible"
+      @click="showAddCategoryInput"
+      v-model="newCategoryName"
+      @click-save="addNewCategory"
+      @keypress-enter="addNewCategory"
+    />
 
     <ul class="navigation__list">
       <li
@@ -53,48 +30,34 @@
         >
           {{ category.name }}
         </router-link>
+
         <base-dropdown
           :options="dropdownOptions"
           @dropdownRename="renameCategory(category._id)"
           @dropdownAddNote="addNote(category._id)"
           @dropdownRemove="removeCategory(category._id)"
-        ></base-dropdown>
+        />
       </li>
     </ul>
 
-    <div class="navigation__bottom" v-show="isMenuActive">
-      <img
-        class="navigation__user-avatar"
-        src="https://image.flaticon.com/icons/svg/848/848006.svg"
-        alt=""
-      />
-      <span class="navigation__user-name">{{
-        $store.state.user.data.username
-      }}</span>
-
-      <button class="navigation__settings-button" @click="logout">
-        <img
-          class="navigation__settings-icon"
-          src="https://image.flaticon.com/icons/svg/2919/2919706.svg"
-          alt=""
-        />
-      </button>
-    </div>
+    <navigation-bottom v-show="isMenuActive" />
   </nav>
 </template>
 
 <script>
 import gsap from 'gsap';
 import { errors } from '@/assets/consts';
-import BaseInput from '@/components/core/BaseInput';
 import BaseDropdown from '@/components/core/BaseDropdown';
 import BaseDialog from '@/components/core/BaseDialog';
+import Hamburger from '@/components/navigation/Hamburger';
+import NavigationBottom from '@/components/navigation/NavigationBottom';
+import AddCategoryField from '@/components/navigation/AddCategoryField';
 
 export default {
   data() {
     return {
       isMenuActive: true,
-      isAddCategoryActive: false,
+      isCategoryFieldVisible: false,
       newCategoryName: '',
       renameData: {
         categoryName: '',
@@ -110,9 +73,11 @@ export default {
     }
   },
   components: {
-    BaseInput,
     BaseDropdown,
-    BaseDialog
+    BaseDialog,
+    Hamburger,
+    NavigationBottom,
+    AddCategoryField
   },
   watch: {
     isMenuActive(state) {
@@ -137,7 +102,7 @@ export default {
         )
       ];
 
-      if (state) animations.every(x => x.play());
+      if (state) animations.forEach(a => a.play());
     }
   },
   methods: {
@@ -146,12 +111,12 @@ export default {
       this.$emit('navigationClosed');
     },
     showAddCategoryInput() {
-      this.isAddCategoryActive = !this.isAddCategoryActive;
+      this.isCategoryFieldVisible = !this.isCategoryFieldVisible;
       this.newCategoryName = '';
 
       gsap.fromTo(
         '.navigation__add-category-input-wrapper',
-        this.isAddCategoryActive ? 0.4 : 0,
+        this.isCategoryFieldVisible ? 0.4 : 0,
         { opacity: 0, x: '-2rem', maxHeight: 0 },
         { opacity: 1, x: 0, maxHeight: 100 }
       );
@@ -170,7 +135,7 @@ export default {
           });
 
           this.newCategoryName = '';
-          this.isAddCategoryActive = false;
+          this.isCategoryFieldVisible = false;
           this.$emit('addedCategory');
         } else {
           this.$store.dispatch('notificationActivate', {
@@ -247,9 +212,6 @@ export default {
       }
 
       this.$store.dispatch('closeDialog');
-    },
-    logout() {
-      this.$store.dispatch('signOut');
     }
   }
 };
@@ -280,84 +242,6 @@ export default {
     @include mq {
       width: 20rem;
       flex-shrink: 0;
-    }
-  }
-
-  &__hamburger {
-    width: 32px;
-    height: 24px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-
-  &__hamburger-line {
-    display: block;
-    width: 100%;
-    height: 3px;
-    background-color: $navigationHamburgerColor;
-    transition: transform 0.2s ease-in-out, opacity 0.3s ease-in-out;
-
-    &:nth-of-type(2) {
-      width: 75%;
-    }
-  }
-
-  &__hamburger--active &__hamburger-line {
-    &:nth-child(1) {
-      transform: rotate(45deg) translate(0.45rem, 0.5rem);
-    }
-    &:nth-child(2) {
-      opacity: 0;
-      transform: translateX(0.5rem);
-    }
-    &:nth-child(3) {
-      transform: rotate(-45deg) translate(0.45rem, -0.5rem);
-    }
-  }
-
-  &__add-category-wrapper {
-    margin-top: 3rem;
-    text-align: right;
-    margin-left: auto;
-  }
-
-  &__add-category-link {
-    font-size: 0.7rem;
-    display: inline-block;
-    color: $navigationAddLinkColor;
-    padding: 0.25rem 0.5rem;
-    border-radius: 0.25rem;
-    transition: border-color 0.2s;
-    border: 1px solid $navigationAddLinkBorderColor;
-
-    &:hover {
-      border-color: $success;
-    }
-  }
-
-  &__add-category-link:hover &__icon {
-    color: $success;
-  }
-
-  &__icon {
-    margin-right: 0.5rem;
-    transition: color 0.2s;
-    transform: translateX(1px);
-  }
-
-  &__add-category-input-wrapper {
-    display: flex;
-    margin-top: 1rem;
-
-    .base__input {
-      margin: 0;
-    }
-    button {
-      color: $primary;
-      height: auto;
-      padding: 0 1rem;
-      font-size: 1rem;
     }
   }
 
@@ -408,31 +292,6 @@ export default {
 
   &__link:hover &__add-button {
     opacity: 1;
-  }
-
-  &__bottom {
-    width: calc(100% - 3rem);
-    position: absolute;
-    bottom: 1rem;
-    display: flex;
-    align-items: center;
-  }
-
-  &__user-name {
-    color: $navigationUserNameColor;
-  }
-
-  &__user-avatar {
-    width: 2rem;
-    margin-right: 1rem;
-  }
-
-  &__settings-button {
-    margin-left: auto;
-  }
-
-  &__settings-icon {
-    width: 2rem;
   }
 }
 
---
+++ client/src/components/navigation/Hamburger.vue
@@ -0,0 +1,59 @@
+<template>
+  <button
+    class="navigation__hamburger"
+    @click="$emit('click')"
+    :class="{ 'navigation__hamburger--active': isMenuActive }"
+  >
+    <span class="navigation__hamburger-line"></span>
+    <span class="navigation__hamburger-line"></span>
+    <span class="navigation__hamburger-line"></span>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    isMenuActive: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.navigation {
+  &__hamburger {
+    width: 32px;
+    height: 24px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  &__hamburger-line {
+    display: block;
+    width: 100%;
+    height: 3px;
+    background-color: $navigationHamburgerColor;
+    transition: transform 0.2s ease-in-out, opacity 0.3s ease-in-out;
+
+    &:nth-of-type(2) {
+      width: 75%;
+    }
+  }
+
+  &__hamburger--active &__hamburger-line {
+    &:nth-child(1) {
+      transform: rotate(45deg) translate(0.45rem, 0.5rem);
+    }
+    &:nth-child(2) {
+      opacity: 0;
+      transform: translateX(0.5rem);
+    }
+    &:nth-child(3) {
+      transform: rotate(-45deg) translate(0.45rem, -0.5rem);
+    }
+  }
+}
+</style>
