f0f51521c0307ca257da5ad711ca18544e89db39
nv7z2 <barankacper2000@gmail.com>
2020-09-20 02:05:49 +0200

    [added] Back button in Page404

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -2,11 +2,31 @@
   <div class="page-404">
     <h1 class="page-404__heading">404</h1>
     This page does not exist
+    <button class="page-404__button" @click="changeRoute">
+      Back to {{ mode }}
+    </button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    mode() {
+      if (this.$store.state.auth.status) {
+        return 'Dashboard';
+      } else {
+        return 'Login';
+      }
+    }
+  },
+  methods: {
+    changeRoute() {
+      const mode = this.mode;
+
+      this.$router.push(mode ? '/dashboard' : '/auth/login');
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
@@ -21,6 +41,20 @@ export default {};
   &__heading {
     margin: 0;
     font-size: 5rem;
+  }
+
+  &__button {
+    margin-top: 2rem;
+    padding: 1rem 2rem;
+    border-radius: 0.25rem;
+    border: 1px solid $page404BorderColor;
+    transition: all 0.2s ease-in-out;
+
+    &:hover {
+      font-weight: bold;
+      color: $page404ButtonColorHover;
+      background-color: $page404ButtonBackgroundHover;
+    }
   }
 }
 </style>
